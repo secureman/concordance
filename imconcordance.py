656f1b6f513ccbd0628268064682cc3f166ea469
Orsted <secureship971@gmail.com>
2021-09-30 14:17:56 +0100

    improved script!

---
+++ imconcordance.py
@@ -0,0 +1,38 @@
+from bs4 import UnicodeDammit
+import chardet
+import os
+import nltk
+import codecs
+import arabic_reshaper
+import bidi.algorithm
+import sys
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+
+def predict_encoding(file_path, n_lines=20):
+    '''Predict a file's encoding using chardet'''
+    import chardet
+
+    # Open the file as binary data
+    with open(file_path, 'rb') as f:
+        # Join binary lines for specified number of lines
+        rawdata = b''.join([f.readline() for _ in range(n_lines)])
+
+    return chardet.detect(rawdata)['encoding']
+
+text = '/home/mac101/python_learn/testing/concordance/Khaleej-2004/Economy/arc_Articlesww0221.html'
+#fili = predict_encoding(filep_ath)
+#print(fili)
+def concordancing(text):
+    with open(text, encoding = predict_encoding(text))as f:
+        for f in f:
+            tokens = nltk.word_tokenize(arabic_reshaper.reshape(f))
+            text = nltk.Text(tokens)
+            text.concordance(input('what word u looking for:' ))
+            print(text.concordance)
+            bcf = BigramCollocationFinder.from_words(tokens)
+            print(bcf.nbest(BigramAssocMeasures.likelihood_ratio, 4))
+   # return()
+concordancing(text)
+
+
